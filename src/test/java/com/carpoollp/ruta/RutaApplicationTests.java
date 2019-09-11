775c0bf053855e73398208d4f8c6100a73a40fca
Rodolfo Catunta <rodolfo.catunta.uturunco@gmail.com>
2019-09-11 17:20:55 -0400

    Iniciando Repositorio

---
+++ src/test/java/com/carpoollp/ruta/RutaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpoollp.ruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
